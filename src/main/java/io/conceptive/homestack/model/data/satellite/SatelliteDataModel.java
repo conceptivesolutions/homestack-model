24c50face53fd5b075009d0df5bc024a6b50f909
w.glanzer <w.glanzer@conceptive.io>
2020-11-30 18:28:14 +0100

    feat: added model for satellite

---
+++ src/main/java/io/conceptive/homestack/model/data/satellite/SatelliteDataModel.java
@@ -0,0 +1,21 @@
+package io.conceptive.homestack.model.data.satellite;
+
+/**
+ * Model for a single satellite
+ *
+ * @author w.glanzer, 30.11.2020
+ */
+public class SatelliteDataModel
+{
+
+  /**
+   * ID of the satellite
+   */
+  public String id;
+
+  /**
+   * ID of the host this satellite belongs to
+   */
+  public String hostID;
+
+}
