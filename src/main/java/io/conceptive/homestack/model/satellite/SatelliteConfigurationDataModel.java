261def6755a7104adb4d4253cfdb3f5e2c9ae9b0
w.glanzer <w.glanzer@conceptive.io>
2021-02-20 17:08:10 +0100

    feat: replaced hashCode/equals with annotation and added builder

--- src/main/java/io/conceptive/homestack/model/satellite/SatelliteConfigurationDataModel.java
+++ src/main/java/io/conceptive/homestack/model/satellite/SatelliteConfigurationDataModel.java
@@ -2,15 +2,18 @@ package io.conceptive.homestack.model.satellite;
 
 import io.conceptive.homestack.model.data.device.DeviceDataModel;
 import io.conceptive.homestack.model.data.metric.MetricDataModel;
+import lombok.*;
 
-import java.util.*;
+import java.util.Set;
 
 /**
  * Model, that a single satellite retrieves from the cloud, to get its configuration.
  *
  * @author w.glanzer, 10.11.2020
  */
-@SuppressWarnings("unused")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SatelliteConfigurationDataModel
 {
 
@@ -25,21 +28,4 @@ public class SatelliteConfigurationDataModel
    */
   public Set<MetricDataModel> metrics;
 
-  @Override
-  public boolean equals(Object pO)
-  {
-    if (this == pO)
-      return true;
-    if (pO == null || getClass() != pO.getClass())
-      return false;
-    SatelliteConfigurationDataModel that = (SatelliteConfigurationDataModel) pO;
-    return Objects.equals(devices, that.devices) &&
-        Objects.equals(metrics, that.metrics);
-  }
-
-  @Override
-  public int hashCode()
-  {
-    return Objects.hash(devices, metrics);
-  }
 }
